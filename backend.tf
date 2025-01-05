e02023f9e6fa05686b008a4fb8ea7ba5f5c8214f
l-u-r <lourdescf@gmail.com>
2025-01-05 01:01:54 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>l-u-r"
+    organization = "l-u-r"
     workspaces {
       name = "ace-iac-day-zero"
     }
